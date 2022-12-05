cd6c434eda539987bdbb582a5dc0fff0f2aa7b45
Xavier Fenton <xavierfenton99@gmail.com>
2022-12-05 13:45:44 +1300

    update version with date | avoid deprecation

--- src/client.js
+++ src/client.js
@@ -3,5 +3,6 @@ import sanityClient from '@sanity/client'
 export default sanityClient({
   projectId: 'p9jh57yb', // find this at manage.sanity.io or in your sanity.json
   dataset: 'production', // this is from those question during 'sanity init'
+  apiVersion: '2021-08-31', // use a UTC date string
   useCdn: true,
 })
