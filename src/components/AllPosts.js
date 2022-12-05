5d872c5550d2c73adc9fae1fad61e3518ee39668
Xavier Fenton <xavierfenton99@gmail.com>
2022-12-05 13:00:04 +1300

    add comments merge this to a dev branch in future

--- src/components/AllPosts.js
+++ src/components/AllPosts.js
@@ -4,14 +4,22 @@ import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import sanityClient from '../client.js'
 
+//  How this works:
+//  We need React Hooks = useState to set the state of our data. Then useEffect  to fetch our data.
+//  Imported link to linking to our single blog posts.
+//  Import the sanityClient so we can fetch our data
+
+// * sanityClient function *
+//an imported function which  we can dot into neccessary things such as fetch. Our fetch is in *strings* (neccesary data).
 export default function AllPosts() {
-  const [allPostsData, setAllPosts] = useState(null)
+  const [allPostsData, setAllPosts] = useState(null) //1. setting our state
 
   useEffect(() => {
+    //2. useEffect to fetch our data.
     sanityClient
       .fetch(
-        `*[_type == "post"]{
-        title,
+        `*[_type == "post"]{ 
+        title, 
         slug,
         mainImage{
           asset->{
