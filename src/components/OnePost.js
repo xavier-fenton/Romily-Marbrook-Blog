35804b8d72dfe038f0f8ac23aa3dbedac3cd8b16
Xavier Fenton <xavierfenton99@gmail.com>
2022-12-08 14:57:05 +1300

    fix image bug not showing

--- src/components/OnePost.js
+++ src/components/OnePost.js
@@ -44,14 +44,18 @@ export default function OnePost() {
       <div>
         <h2>{postData.title}</h2>
         <div>
-          <img
-            src={urlFor(postData.authorImage).width(100).url()}
-            alt="Author is Kap"
-          />
+          {!postData.authorImage || (
+            <img
+              src={urlFor(postData.authorImage).width(100).url()}
+              alt="Author is Kap"
+            />
+          )}
           <h4>{postData.name}</h4>
         </div>
       </div>
-      <img src={urlFor(postData.mainImage).width(200).url()} alt="" />
+      {!postData.mainImage || (
+        <img src={urlFor(postData.mainImage).width(200).url()} alt="" />
+      )}
       <div>
         <BlockContent
           blocks={postData.body}
