590cb1940ef4a256c04a99aa75f03f8b1c0f61d1
Xavier Fenton <xavierfenton99@gmail.com>
2023-01-10 08:29:02 +1300

    reword component to suit

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import '../css/mainmenu.scss'
+import AllPosts from './AllPosts'
 
 const MainMenu = () => {
-  return <p>ROMILYmarbrook</p>
+  return (
+    <>
+      <p>ROMILYmarbrook</p>
+      <AllPosts />
+    </>
+  )
 }
 
 export default MainMenu
