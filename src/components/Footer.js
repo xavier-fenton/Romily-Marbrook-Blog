90075dbee9b88de9dfed50b91dcb7a489c9e848b
Xavier Fenton <xavierfenton99@gmail.com>
2023-01-10 16:38:01 +1300

    create footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../css/footer.scss'
+const Footer = () => {
+  return (
+    <div className="footer-component">
+      <p>Footer component</p>
+      {/* Own Component */}
+    </div>
+  )
+}
+
+export default Footer
