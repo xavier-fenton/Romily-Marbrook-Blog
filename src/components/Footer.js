cea97c3ebc6415eb3ded221c74206075bc7038c1
Xavier Fenton <xavierfenton99@gmail.com>
2023-01-10 18:20:29 +1300

    hard coded values

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,15 +3,23 @@ import '../css/footer.scss'
 const Footer = () => {
   return (
     <div className="footer-component">
-      <p className="footer-content">Name</p>
-      <p className="footer-content">Romily Marbrook</p>
-      <p className="footer-content">Phone</p>
-      <p className="footer-content">023 23123 323</p>
-      <p className="footer-content">Email</p>
-      <p className="footer-content">romily@gmail.com</p>
-      <p className="footer-content">Instagram</p>
-      <p className="footer-content">rom1ly</p>
-      {/* Own Component */}
+      <div className="footer-dataline">
+        <p className="footer-content">Name</p>
+        <p className="footer-content">Romily Marbrook</p>
+      </div>
+      <div className="footer-dataline">
+        <p className="footer-content">Phone</p>
+        <p className="footer-content">023 23123 323</p>
+      </div>
+      <div className="footer-dataline">
+        <p className="footer-content">Email</p>
+        <p className="footer-content">romily@gmail.com</p>
+      </div>
+      <div className="footer-dataline">
+        <p className="footer-content">Instagram</p>
+        <p className="footer-content">@rom1ly</p>{' '}
+        {/* could possibly have instagram handle changable with changing data via cms*/}
+      </div>
     </div>
   )
 }
