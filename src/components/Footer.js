e52a8793dbff2387d953bcb9e62940b545b1e38c
Xavier Fenton <xavierfenton99@gmail.com>
2023-01-10 17:29:05 +1300

    dont like the look of this mark up -hardcoded

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,10 +3,14 @@ import '../css/footer.scss'
 const Footer = () => {
   return (
     <div className="footer-component">
-      <p className="footer-content">Name Romily Marbrook</p>
-      <p className="footer-content">Phone 0212345667</p>
-      <p className="footer-content">Email romily@gmail.com</p>
-      <p className="footer-content">Instagram @rom1ly</p>
+      <p className="footer-content">Name</p>
+      <p className="footer-content">Romily Marbrook</p>
+      <p className="footer-content">Phone</p>
+      <p className="footer-content">023 23123 323</p>
+      <p className="footer-content">Email</p>
+      <p className="footer-content">romily@gmail.com</p>
+      <p className="footer-content">Instagram</p>
+      <p className="footer-content">rom1ly</p>
       {/* Own Component */}
     </div>
   )
