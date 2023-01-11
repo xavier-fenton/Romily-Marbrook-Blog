bb0a95a6a1e251fcbcebb40a4ebd287cc1f67d7e
Xavier Fenton <xavierfenton99@gmail.com>
2023-01-12 09:12:39 +1300

    remove comments

--- src/components/AllPosts.js
+++ src/components/AllPosts.js
@@ -6,24 +6,16 @@ import { Link } from 'react-router-dom'
 import sanityClient from '../client.js'
 import '../App.scss'
 
-//  How this works:
-//  We need React Hooks = useState to set the state of our data. Then useEffect  to fetch our data.
-//  Imported link to linking to our single blog posts.
-//  Import the sanityClient so we can fetch our data
-
-// * sanityClient function *
-//an imported function which  we can dot into neccessary things such as fetch. Our fetch is in *strings* (neccesary data).
-
 const builder = imageUrlBuilder(sanityClient)
 function urlFor(source) {
   return builder.image(source)
 }
 
 export default function AllPosts() {
-  const [allPostsData, setAllPosts] = useState() //1. setting our state
+  const [allPostsData, setAllPosts] = useState() 
 
   useEffect(() => {
-    //2. useEffect to fetch our data.
+    
     sanityClient
       .fetch(
         `*[_type == "post"]{ 
@@ -68,7 +60,7 @@ export default function AllPosts() {
                     <div className="title-col-content">
                       {post.createdAtFormatted}
 
-                      {/* make a funtion which converts the date*/}
+                      
                     </div>
                   </div>
                 </span>
