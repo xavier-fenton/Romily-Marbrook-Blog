15dd81a30149e63b19c5f230b9af0d2d1018477d
Xavier Fenton <xavierfenton99@gmail.com>
2023-02-07 17:34:15 +1300

    connect data

--- src/components/subcomponents/LandingPage.js
+++ src/components/subcomponents/LandingPage.js
@@ -1,7 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import sanityClient from '../../client'
+import BlockContent from '@sanity/block-content-to-react'
+import imageUrlBuilder from '@sanity/image-url'
+
+const builder = imageUrlBuilder(sanityClient)
+function urlFor(source) {
+  return builder.image(source)
+}
 
 const LandingPage = () => {
-  return <div>hello</div>
+  const [postData, setPostData] = useState(null)
+  // const { slug } = useParams()
+
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[_type == 'landingpage']{
+          title,
+          body,
+       }`
+      )
+      .then((data) => setPostData(data[0]))
+      .catch(console.error)
+  }, [])
+
+  if (!postData) return <div>loading....</div>
+
+  return (
+    <>
+      <h1>{postData.title}</h1>
+      <div>
+        <BlockContent
+          blocks={postData.body}
+          projectId={sanityClient.clientConfig.projectId}
+          dataset={sanityClient.clientConfig.dataset}
+        />
+      </div>
+    </>
+  )
 }
 
 export default LandingPage
