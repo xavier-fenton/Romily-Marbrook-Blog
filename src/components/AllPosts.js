ebb19a18c04d00a155f37193d8074a4e7fd6d8c8
Xavier Fenton <xavierfenton99@gmail.com>
2023-03-23 15:45:06 +1300

    create a child component

--- src/components/AllPosts.js
+++ src/components/AllPosts.js
@@ -1,37 +1,37 @@
 // src/components/AllPosts.js
 
-import imageUrlBuilder from '@sanity/image-url'
 import React, { useEffect, useState } from 'react'
-import { Link, useLocation } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
 import sanityClient from '../client.js'
 import '../App.scss'
+import Child from './subcomponents/Child.js'
 
-const builder = imageUrlBuilder(sanityClient)
-function urlFor(source) {
-  return builder.image(source)
-}
+// const builder = imageUrlBuilder(sanityClient)
+// function urlFor(source) {
+//   return builder.image(source)
+// }
 
 export default function AllPosts() {
   const location = useLocation()
   const [allPostsData, setAllPosts] = useState()
-  const [boolState, setBoolState] = useState('false')
+  // const [boolState, setBoolState] = useState('false')
 
-  function handleClick() {
-    let doc = document.getElementById('land-cont')
-    // console.log(doc)
-    doc.style.display = 'none'
-    let gifLoader = document.getElementById('gif-loader')
-    gifLoader.style.display = 'block'
-    let leftDoc = document.getElementById('left-contain')
+  // function handleClick() {
+  //   let doc = document.getElementById('land-cont')
+  //   // console.log(doc)
+  //   doc.style.display = 'none'
+  //   let gifLoader = document.getElementById('gif-loader')
+  //   gifLoader.style.display = 'block'
+  //   let leftDoc = document.getElementById('left-contain')
 
-    if (boolState === 'false') {
-      leftDoc.style.width = '8%'
-      setBoolState('true')
-    } else if (boolState === 'true') {
-      leftDoc.style.width = '450px'
-      setBoolState('false')
-    }
-  }
+  //   if (boolState === 'false') {
+  //     leftDoc.style.width = '8%'
+  //     setBoolState('true')
+  //   } else if (boolState === 'true') {
+  //     leftDoc.style.width = '450px'
+  //     setBoolState('false')
+  //   }
+  // }
 
   useEffect(() => {
     sanityClient
@@ -63,24 +63,16 @@ export default function AllPosts() {
             const active = location.pathname === to
 
             return (
-              <Link className="Links" to={to} key={post.slug.current}>
-                <span key={index}>
-                  <div
-                    className={`left-col-dataline ${
-                      active ? 'active' : 'hover-1'
-                    } `}
-                    id="leftcoldataline"
-                    onClick={handleClick}
-                    bool={boolState}
-                  >
-                    <div className="title-col-content">{post.title}</div>
-                  </div>
-                </span>
+              <Child
+                post={post}
+                index={index}
+                to={to}
+                active={active}
 
-                {!post.mainImage || (
-                  <img src={urlFor(post.mainImage).width(250).url()} alt="" />
-                )}
-              </Link>
+                // boolState={boolState}
+                // setBoolState={setBoolState}
+                // handleClick={handleClick}
+              />
             )
           })
         : null}
